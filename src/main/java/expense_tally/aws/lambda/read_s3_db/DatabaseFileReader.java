9dad46e685e65e5e988631af3bc497a768558315
boyshawn <boyshawn@hotmail.com>
2020-12-20 00:32:38 +0800

    Work in project; reading from database

---
+++ src/main/java/expense_tally/aws/lambda/read_s3_db/DatabaseFileReader.java
@@ -0,0 +1,4 @@
+package expense_tally.aws.lambda.read_s3_db;
+
+public class DatabaseFileReader {
+}
--- src/main/java/expense_tally/aws/lambda/read_s3_db/app/App.java
+++ src/main/java/expense_tally/aws/lambda/read_s3_db/app/App.java
@@ -2,21 +2,20 @@ package expense_tally.aws.lambda.read_s3_db.app;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
+import expense_tally.aws.lambda.read_s3_db.DatabaseS3EventAnalyzer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.HashMap;
-
-public class App implements RequestHandler<S3Event, Void> {
+public class App implements RequestHandler<S3Event, String> {
   private static final Logger LOGGER = LogManager.getLogger(App.class);
 
   @Override
-  public Void handleRequest(S3Event event, Context context) {
+  public String handleRequest(S3Event event, Context context) {
     LOGGER.atInfo().log("Received a new S3 event: {}", event);
     // Check if the name of the resource changed is what I am looking out for
-
+    DatabaseS3EventAnalyzer databaseS3EventAnalyzer = new DatabaseS3EventAnalyzer("personal.db");
+    databaseS3EventAnalyzer.extractDatabaseObjectKey(event);
     // Read the type of S3 event
 
     // Read file content

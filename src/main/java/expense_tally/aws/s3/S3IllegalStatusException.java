b56f1508e01faa4f8c2882a582a7221f47d1a4aa
boyshawn <boyshawn@hotmail.com>
2021-05-29 20:31:46 +0800

    Add Serializable to exception classes

--- src/main/java/expense_tally/aws/s3/S3IllegalStatusException.java
+++ src/main/java/expense_tally/aws/s3/S3IllegalStatusException.java
@@ -1,6 +1,8 @@
 package expense_tally.aws.s3;
 
-public class S3IllegalStatusException extends IllegalStateException {
+import java.io.Serializable;
+
+public class S3IllegalStatusException extends IllegalStateException implements Serializable {
   public S3IllegalStatusException(String message) {
     super(message);
   }
